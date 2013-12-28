fb952bfcd69829ebe43842e4a32df80db9cdf86f
Michael Quinlan <mquinlan@sportslabs.com>
2013-12-28 15:55:01 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChildSupport::Application.config.secret_token = '206a0aff45ea3d31e23c2d9fe42f7894e76ce814d2f24480ed099e4477ed227f3e8926e13503b7f1f47d41ab44e1dcd31b1476bc7f4c9b996111c3d9200e1362'
